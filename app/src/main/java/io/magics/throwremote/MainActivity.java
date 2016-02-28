e558c7a219a3b215558869b46b473b289731dc98
Paul Sajna <paulsajna@gmail.com>
2016-02-28 12:59:58 -0800

    fuuuuuuuuuuuuuuck

--- app/src/main/java/io/magics/throwremote/MainActivity.java
+++ app/src/main/java/io/magics/throwremote/MainActivity.java
@@ -20,27 +20,28 @@ import com.github.nkzawa.socketio.client.Socket;
 
 import java.net.URISyntaxException;
 import java.lang.Math;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.concurrent.TimeUnit;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
     private SeekBar mSeekbar;
+    private int mPositionValue;
     private TextView mTimeTextView;
+    private TextView mLengthTextView;
+    private TextView mTitleTextView;
     private ImageButton mPlayImageButton;
-    private Boolean mPlaying = true;
+    private Boolean mPlaying;
     private ImageButton mMuteImageButton;
-    private Boolean mMuted = false;
-
+    private Boolean mMuted;
+    private ImageButton mForwardImageButton;
+    private ImageButton mBackwardImageButton;
+    private ImageButton mStopButton;
+    private ImageView mImageView;
     private Socket mSocket;
-    {
-        try {
-            mSocket = IO.socket("http://10.19.223.8:1337");
-        } catch (URISyntaxException e) {}
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +50,38 @@ public class MainActivity extends AppCompatActivity {
 
         mSeekbar = (SeekBar) findViewById(R.id.seekBar2);
         mTimeTextView = (TextView) findViewById(R.id.textView);
+        mLengthTextView = (TextView) findViewById(R.id.textView2);
+        mTitleTextView = (TextView) findViewById(R.id.textView3);
         mPlayImageButton = (ImageButton) findViewById(R.id.imageButton2);
+        mMuteImageButton = (ImageButton) findViewById(R.id.imageButton4);
+        mForwardImageButton = (ImageButton) findViewById(R.id.imageButton5);
+        mBackwardImageButton = (ImageButton) findViewById(R.id.imageButton3);
+        mStopButton = (ImageButton) findViewById(R.id.imageButton7);
+        mImageView = (ImageView) findViewById(R.id.imageView);
+
+        mPositionValue = 0;
+        mMuted = false;
+        mPlaying = true;
+
+        /*mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mSocket.emit("position", progress / 10000.0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });*/
+
+
         mPlayImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,14 +92,16 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         if (mPlaying) {
                             mPlayImageButton.setBackgroundResource(R.drawable.ic_pause_circle_outline_64dp);
+                            Log.i("pause","pause");
                         } else {
                             mPlayImageButton.setBackgroundResource(R.drawable.ic_play_circle_outline_64dp);
+                            Log.i("play", "play");
                         }
                     }
                 });
             }
         });
-        mMuteImageButton = (ImageButton) findViewById(R.id.imageButton5);
+
 
         mMuteImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,12 +121,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mSocket.on("position", onPositionUpdate);
-        mSocket.on("time", onTimeUpdate);
-        mSocket.on("playing", onPlayToggle);
-        mSocket.on("muted", onMuteToggle);
-        mSocket.on("ended", onEnded);
-        mSocket.connect();
+        mForwardImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double percent = mPositionValue / 10000.0;
+                Log.i("percent", String.valueOf(percent));
+                mSocket.emit("forward", percent);
+            }
+        });
+
+        mBackwardImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double percent = mPositionValue / 10000.0;
+                Log.i("percent", String.valueOf(percent));
+                mSocket.emit("backward", percent);
+            }
+        });
+
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("end", "end");
+                mSocket.emit("ended");
+            }
+        });
+
     }
 
     @Override
@@ -155,9 +209,36 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 0) {
 
             if (resultCode == RESULT_OK) {
-                String contents = data.getStringExtra("SCAN_RESULT");
-                Log.i("qr", contents);
+                String json = data.getStringExtra("SCAN_RESULT");
+                try {
+                    JSONObject jsonObj = new JSONObject(json);
+                    String ip = jsonObj.getString("ip");
+                    int port = jsonObj.getInt("port");
+                    int pin = jsonObj.getInt("pin");
+                    String uri = "http://" + ip + ":" + String.valueOf(port);
+                    Log.i("uri", uri);
+                    Log.i("pin", String.valueOf(pin));
+                    try {
+                        mSocket = IO.socket(uri);
+                        mSocket.connect();
+                        mSocket.emit("pin", pin);
+                        mSocket.on("title", onTitleUpdate);
+                        mSocket.on("length", onLengthUpdate);
+                        mSocket.on("position", onPositionUpdate);
+                        mSocket.on("time", onTimeUpdate);
+                        mSocket.on("playing", onPlayToggle);
+                        mSocket.on("muted", onMuteToggle);
+                        mSocket.on("ended", onEnded);
 
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mImageView.setImageResource(R.drawable.ic_done_256dp);
+                            }
+                        });
+
+                    } catch (Exception e) {e.printStackTrace();}
+                } catch (JSONException e) {e.printStackTrace();}
             }
             if(resultCode == RESULT_CANCELED){
                 //handle cancel
@@ -165,16 +246,49 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private Emitter.Listener onTitleUpdate = new Emitter.Listener() {
+        @Override
+        public void call(Object ... args) {
+            final String title = (String) args[0];
+            Log.i("title", title);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mTitleTextView.setText(title);
+                }
+            });
+        }
+    };
+
+    private Emitter.Listener onLengthUpdate = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            Integer length = (Integer) args[0];
+            Log.i("length", length.toString());
+            final String dateFormatted = String.format("%02d:%02d:%02d",
+                    TimeUnit.MILLISECONDS.toHours(length),
+                    TimeUnit.MILLISECONDS.toMinutes(length) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(length)),
+                    TimeUnit.MILLISECONDS.toSeconds(length) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(length)));
+            Log.i("length", dateFormatted);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mLengthTextView.setText(dateFormatted);
+                }
+            });
+        }
+    };
+
     private Emitter.Listener onPositionUpdate = new Emitter.Listener() {
         @Override
         public void call(Object... args) {
             Double position = (Double) args[0];
-            Log.i("position", position.toString());
-            final int positionPercent = (int) Math.round(position * 100);
+            //Log.i("position", position.toString());
+            mPositionValue = (int) Math.round(position * 10000);
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    mSeekbar.setProgress(positionPercent);
+                    mSeekbar.setProgress(mPositionValue);
                 }
             });
         }
@@ -184,8 +298,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void call(Object... args) {
             Integer time = (Integer) args[0];
-            Log.i("time", time.toString());
-
+            //Log.i("time", time.toString());
             final String dateFormatted = String.format("%02d:%02d:%02d",
                     TimeUnit.MILLISECONDS.toHours(time),
                     TimeUnit.MILLISECONDS.toMinutes(time) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(time)),
@@ -238,10 +351,13 @@ public class MainActivity extends AppCompatActivity {
     private Emitter.Listener onEnded = new Emitter.Listener() {
         @Override
         public void call(Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mImageView.setImageResource(R.drawable.ic_cloud_off_256dp);
+                }
+            });
             Log.i("ended", "ended");
         }
     };
-
-
-
 }
